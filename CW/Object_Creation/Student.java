3183dfa5f3bd104d92e8e6760812b5e77c2474f9
shubhammore34 <shubhammore8338@gmail.com>
2025-04-21 22:16:54 +0530

    Create Student.java

---
+++ CW/Object_Creation/Student.java
@@ -0,0 +1,13 @@
+package Object_Creation;
+
+public class Student {
+    int roll_no;
+    String name;
+    int marks;
+    
+    void display() {
+    	System.out.println(roll_no);
+    	System.out.println(name);
+    	
+    }
+}
