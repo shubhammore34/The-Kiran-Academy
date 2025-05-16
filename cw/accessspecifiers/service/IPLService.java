ebb0af8e63aabb33aa5609e56d31979e3cabee4f
shubhammore34 <shubhammore8338@gmail.com>
2025-05-16 20:41:35 +0530

    Create IPLService.java

---
+++ cw/accessspecifiers/service/IPLService.java
@@ -0,0 +1,9 @@
+package cw.accessspecifiers.service;
+public class IPLService {
+    private static int totalteams = 10;
+
+    public void displayTeams() {
+        System.out.println(totalteams);
+    }
+    
+}
