1404ef28bdb368fefa881aadf78d4e9a4a9e9db5
shubhammore34 <shubhammore8338@gmail.com>
2025-05-18 22:41:35 +0530

    initial commit

--- cw/accessspecifiers/service/IPLService.java
+++
@@ -1,9 +0,0 @@
-package cw.accessspecifiers.service;
-public class IPLService {
-    private static int totalteams = 10;
-
-    public void displayTeams() {
-        System.out.println(totalteams);
-    }
-    
-}
